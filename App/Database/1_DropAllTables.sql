1c8984e2ae14e7e9cac8f374ad331e04c845137c
sumitshevade <sumitshevade@gmail.com>
2020-04-04 12:55:19 +0530

    Refactored: Database.

---
+++ App/Database/1_DropAllTables.sql
@@ -0,0 +1,46 @@
+USE PublicData
+GO
+
+/*
+DROP TABLE ProgramAttendance;
+DROP TABLE Ticket;
+DROP TABLE Program;
+DROP TABLE University;
+DROP TABLE PersonWorkExperience;
+DROP TABLE PersonSocialMediaAccount;
+DROP TABLE PersonLanguage;
+DROP TABLE PersonHobbyFavorite;
+DROP TABLE PersonHealthDetails;
+DROP TABLE PersonFamilyDetails;
+DROP TABLE PersonEducation;
+DROP TABLE PersonDisability;
+DROP TABLE PersonContact;
+DROP TABLE PersonAddress;
+DROP TABLE PersonAchievement;
+DROP TABLE PersonPrivateInformation;
+DROP TABLE DivisionHead;
+DROP TABLE Division;
+DROP TABLE Organization;
+DROP TABLE DepartmentHead;
+DROP TABLE Person;
+DROP TABLE Details;
+DROP TABLE Department;
+DROP TABLE Header;
+DROP TABLE City;
+DROP TABLE State;
+DROP TABLE Country;
+*/
+
+--DROP TABLE School;
+--DROP TABLE Industry;
+
+
+/*
+DROP TABLE [AspNetUserTokens]
+DROP TABLE [AspNetUsers]
+DROP TABLE [AspNetUserRoles]
+DROP TABLE[AspNetUserLogins]
+DROP TABLE [AspNetUserClaims]
+DROP TABLE [AspNetRoles]
+DROP TABLE [AspNetRoleClaims]
+*/
