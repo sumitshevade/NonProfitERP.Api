d7faaee032b64d10c4c712e98a41284a69f9c726
sumitshevade <sumitshevade@gmail.com>
2020-08-10 21:26:14 +0530

    Added repositories for master tables.

---
+++ PublicData.DAL/Repository/DepartmentHeadRepository.cs
@@ -0,0 +1,12 @@
+﻿using PublicData.DAL.Entities;
+using PublicData.DAL.Interfaces;
+
+namespace PublicData.DAL.Repository
+{
+    public class DepartmentHeadRepository : Repository<DepartmentHead>, IDepartmentHeadRepository
+    {
+        public DepartmentHeadRepository(PublicDataContext context) : base(context)
+        {
+        }
+    }
+}
