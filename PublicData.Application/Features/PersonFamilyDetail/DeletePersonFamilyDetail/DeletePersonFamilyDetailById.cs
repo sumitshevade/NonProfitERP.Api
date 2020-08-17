47d4ffba53cd1fb92297d5b037c0ecc69eef616a
sumitshevade <sumitshevade@gmail.com>
2020-08-17 18:30:27 +0530

    Added delete commands.

---
+++ PublicData.Application/Features/PersonFamilyDetail/DeletePersonFamilyDetail/DeletePersonFamilyDetailById.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using PublicData.Common.Exceptions;
+using PublicData.Common.Interfaces;
+using PublicData.DAL.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PublicData.Application.Features.PersonFamilyDetail.DeletePersonFamilyDetail
+{
+    using DAL.Entities;
+
+    public class DeletePersonFamilyDetailByIdCommandHandler : IRequestHandler<DeletePersonFamilyDetailByIdCommand, bool>
+    {
+        private readonly IPersonFamilyDetailsRepository _personFamilyDetailsRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeletePersonFamilyDetailByIdCommandHandler(IPersonFamilyDetailsRepository personFamilyDetailsRepository, IUnitOfWork unitOfWork)
+        {
+            _personFamilyDetailsRepository = personFamilyDetailsRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<bool> Handle(DeletePersonFamilyDetailByIdCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _personFamilyDetailsRepository.GetById(request.FamilyMemberId);
+                if (entity == null)
+                {
+                    throw new NotFoundException(nameof(PersonEducation), request.FamilyMemberId);
+                }
+
+                entity.IsActive = false;
+                _personFamilyDetailsRepository.Update(entity);
+
+                return Task.FromResult(_unitOfWork.Commit());
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+
+    public class DeletePersonFamilyDetailByIdCommand : IRequest<bool>
+    {
+        public int FamilyMemberId { get; set; }
+    }
+}
---
+++ PublicData.Application/Features/PersonSocialMediaAccount/DeletePersonSocialMediaAccount/DeletePersonSocialMediaAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using PublicData.DAL.Interfaces;
+using PublicData.Common.Interfaces;
+using PublicData.Common.Exceptions;
+
+namespace PublicData.Application.Features.PersonSocialMediaAccount.DeletePersonSocialMediaAccount
+{
+    using DAL.Entities;
+
+    public class DeletePersonSocialMediaAccountByIdCommandHandler : IRequestHandler<DeletePersonSocialMediaAccountByIdCommand, bool>
+    {
+        private readonly IPersonSocialMediaAccountRepository _personSocialMediaAccountRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeletePersonSocialMediaAccountByIdCommandHandler(IPersonSocialMediaAccountRepository personSocialMediaAccountRepository, IUnitOfWork unitOfWork)
+        {
+            _personSocialMediaAccountRepository = personSocialMediaAccountRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<bool> Handle(DeletePersonSocialMediaAccountByIdCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _personSocialMediaAccountRepository.GetById(request.SocialMediaAccountId);
+                if (entity == null)
+                {
+                    throw new NotFoundException(nameof(PersonEducation), request.SocialMediaAccountId);
+                }
+
+                entity.IsActive = false;
+                _personSocialMediaAccountRepository.Update(entity);
+
+                return Task.FromResult(_unitOfWork.Commit());
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+
+    public class DeletePersonSocialMediaAccountByIdCommand : IRequest<bool>
+    {
+        public int SocialMediaAccountId { get; set; }
+    }
+}
