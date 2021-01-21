5c687bc3bcf6cc6d0ddef6397e7a247672b289b7
sumitshevade <sumit.shevade.ext@gravito.net>
2021-01-21 19:32:28 +0530

    Using local apis.

--- PublicData.WebClient/Program.cs
+++ PublicData.WebClient/Program.cs
@@ -25,8 +25,8 @@ namespace PublicData.WebClient
             //    builder.Configuration.Bind("Local", options.ProviderOptions);
             //});
 
-            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44333/") });
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://peoplemanagement-api.azurewebsites.net/") });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44333/") });
+            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://peoplemanagement-api.azurewebsites.net/") });
 
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddBlazoredSessionStorage(options =>
