224d45083027eed5458af440fbdcb4c004ab99de
sumitshevade <sumit.shevade.ext@gravito.net>
2021-03-04 18:15:46 +0530

    Batch master added.

---
+++ PublicData.Application/Features/Master/Batch/DeleteBatch/DeleteBatch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using PublicData.DAL.Interfaces;
+using PublicData.Common.Exceptions;
+using PublicData.Common.Interfaces;
+
+namespace PublicData.Application.Features.Master.Batch.DeleteBatch
+{
+    using DAL.Entities;
+
+    public class DeleteBatchCommandHandler : IRequestHandler<DeleteBatchCommand, bool>
+    {
+        private readonly IBatchRepository _batchRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteBatchCommandHandler(IBatchRepository batchRepository, IUnitOfWork unitOfWork)
+        {
+            _batchRepository = batchRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<bool> Handle(DeleteBatchCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _batchRepository.GetById(request.Id);
+
+                if (entity == null)
+                {
+                    throw new NotFoundException(nameof(Batch), request.Id);
+                }
+
+                entity.IsActive = false;
+                _batchRepository.Update(entity);
+                _unitOfWork.Commit();
+
+                return Task.FromResult(true);
+            }
+            catch (Exception)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+
+    public class DeleteBatchCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ PublicData.Application/Features/Master/Batch/GetBatchesByCourseId/GetBatchesByCourseId.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using AutoMapper;
+using System.Threading;
+using System.Threading.Tasks;
+using PublicData.DAL.Interfaces;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PublicData.Application.Shared;
+using AutoMapper.QueryableExtensions;
+
+namespace PublicData.Application.Features.Master.Batch.GetBatchesByCourseId
+{
+    public class GetBatchesByCourseIdQuery : IRequestHandler<GetBatchesByCourseId, IList<BatchModel>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IBatchRepository _batchRepository;
+
+        public GetBatchesByCourseIdQuery(IBatchRepository batchRepository, IMapper mapper)
+        {
+            _batchRepository = batchRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<BatchModel>> Handle(GetBatchesByCourseId request, CancellationToken cancellationToken)
+        {
+            return await _batchRepository.GetList(x => x.CourseId == request.CourseId && x.IsActive == true)
+                .ProjectTo<BatchModel>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+        }
+    }
+
+    public class GetBatchesByCourseId : IRequest<IList<BatchModel>>
+    {
+        public int CourseId { get; set; }
+    }
+}
