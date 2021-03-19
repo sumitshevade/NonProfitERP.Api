3c3a03e091e78dbb6ffcff64526b4f273e8e1b1d
sumitshevade <sumit.shevade.ext@gravito.net>
2021-03-19 11:15:31 +0530

    Added get all details api.

---
+++ PublicData.Application/Features/Master/Detail/GetDetails/GetDetailsQuery.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using PublicData.DAL.Interfaces;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using PublicData.Application.Shared;
+
+namespace PublicData.Application.Features.Master.Detail.GetDetails
+{
+    public class GetDetailsQueryHandler : IRequestHandler<GetDetailsQuery, IList<DetailModel>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IDetailRepository _detailRepository;
+
+        public GetDetailsQueryHandler(IDetailRepository detailRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _detailRepository = detailRepository;
+        }
+
+        public async Task<IList<DetailModel>> Handle(GetDetailsQuery request, CancellationToken cancellationToken)
+        {
+            return await _detailRepository.GetList(x => x.IsActive == true)
+                .ProjectTo<DetailModel>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+        }
+    }
+
+    public class GetDetailsQuery : IRequest<IList<DetailModel>>
+    {
+    }
+}
